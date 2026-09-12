ecdd809866acba4f383e01890c6e8076279b4da5
Aswin M <50163512+asnzodiac@users.noreply.github.com>
2026-09-12 21:06:36 +0530

    Clean up config.js by removing comments
    Removed unnecessary comments and whitespace.

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-// config.js
 const CONFIG = {
-    GOOGLE_MAPS_API_KEY: "AIzaSyBiURTGf2q9_DVCHuYw_6UwnXmNGwKeX_I",
-    AIRPORT_CODE: "COK",
-    REFRESH_INTERVAL: 15000 // 15 seconds
+    GOOGLE_MAPS_API_KEY: "AIzaSyBiURTGf2q9_DVCHuYw_6UwnXmNGwKeX_I"
 };
+   
